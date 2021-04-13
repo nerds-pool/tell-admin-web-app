8d7e0d419729d960439a8f26594f94abe3841043
kaveenhyacinth <alwiskaveen@gmail.com>
2021-04-13 12:36:55 +0530

    merge category filter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { Switch, Route } from "react-router-dom"
+import logo from "./logo.svg";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
 
 // navBar
- /*top*/  import NavBar from "./components/navigationTop/NavBar"
-/*right*/ import SideNavBar from "./components/navigationSide/SideNav"
+/*top*/ import NavBar from "./components/navigationTop/NavBar";
+/*right*/ import SideNavBar from "./components/navigationSide/SideNav";
+/*filter*/ import CategoryFilter from "./components/navigationSide/CategoryFilter";
 
 // Pages - Admin
-import AdminHomePage from "./webPages/Admin/HomePageAdmin"
-import AdminClosedListPage from "./webPages/Admin/ClosedListPageAdmin"
-import AdminOpenListPage from "./webPages/Admin/OpenListPageAdmin"
-import AdminProgressListPage from "./webPages/Admin/ProgressListPageAdmin"
-import RejectedListPage from "./webPages/Admin/RejectedListAdmin"
+import AdminHomePage from "./webPages/Admin/HomePageAdmin";
+import AdminClosedListPage from "./webPages/Admin/ClosedListPageAdmin";
+import AdminOpenListPage from "./webPages/Admin/OpenListPageAdmin";
+import AdminProgressListPage from "./webPages/Admin/ProgressListPageAdmin";
+import RejectedListPage from "./webPages/Admin/RejectedListAdmin";
 
 function App() {
   return (
@@ -24,25 +25,28 @@ function App() {
         {/* body and it's content */}
         <div className="body">
           {/* Sidenavbar */}
-          <div className="sideNav"> <SideNavBar /> </div>
+          <div className="sideNav">
+            {" "}
+            <SideNavBar />{" "}
+          </div>
           {/* body and footer */}
           <div className="comnfot">
             {/* load web page content */}
             <div className="mainContent">
               <Switch>
-                <Route path='/' exact>
+                <Route path="/" exact>
                   <AdminHomePage />
                 </Route>
-                <Route path='/openList'>
+                <Route path="/openList">
                   <AdminOpenListPage />
                 </Route>
-                <Route path='/progressList'>
+                <Route path="/progressList">
                   <AdminProgressListPage />
                 </Route>
-                <Route path='/closedList'>
+                <Route path="/closedList">
                   <AdminClosedListPage />
                 </Route>
-                <Route path='/rejectedList'>
+                <Route path="/rejectedList">
                   <RejectedListPage />
                 </Route>
               </Switch>
@@ -51,7 +55,9 @@ function App() {
             <div className="footer"></div>
           </div>
           {/* filter */}
-          <div className="filter"> bye</div>
+          <div className="filter">
+            <CategoryFilter />
+          </div>
         </div>
       </div>
     </div>
